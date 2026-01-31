2ee34a5610fef68211caae74fd9545208de73451
JK Coding HUB <jitkarmakar001@gmail.com>
2026-01-31 15:20:54 +0530

    Initial commit - React_Project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef, useState } from "react";
+import { Clock } from "./components/clock";
+import "./style.css";
+
+export default function App () {
+  const containerRef = useRef(null);
+  const [dimens, setDimens] = useState({ width: 0, height: 0 });
+
+  const viewBox = [0, 0, dimens.w, dimens.h].join(" ")
+
+  useEffect(() => {
+    setDimens({
+      w: containerRef.current.getBoundingClientRect().width,
+      h: containerRef.current.getBoundingClientRect().height
+    });
+  }, [containerRef]);
+
+  return (
+    <div className="App" ref={containerRef}>
+      <Clock dimens={dimens} viewBox={viewBox} />
+    </div>
+  );
+}
+
