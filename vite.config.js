2c7f3d99f416788bb2284bc46b08e48d6f91c730
JK Coding HUB <jitkarmakar001@gmail.com>
2026-02-01 17:32:10 +0530

    Fix allowedHosts for Render

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: "all",
+    server: {
+    allowedHosts: "https://analog-clock-g0wk.onrender.com",
   },
-});
+})
